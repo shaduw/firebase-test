30fc1a104ed9a6cf94fa1368a1d46ff49909e5e6
Z <zz@Zs-MacBook-Pro-2.local>
2022-11-24 19:31:59 -0800

    initial commit

---
+++ src/firebase.js
@@ -0,0 +1,18 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyCXdYbvPL-nQyoIqtReEdILHjB7mt8P2vM",
+  authDomain: "infinitypool-3d21d.firebaseapp.com",
+  databaseURL: "https://infinitypool-3d21d.firebaseio.com",
+  projectId: "infinitypool-3d21d",
+  storageBucket: "infinitypool-3d21d.appspot.com",
+  messagingSenderId: "504686720962",
+  appId: "1:504686720962:web:bf517464b1e777b464c85b"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
